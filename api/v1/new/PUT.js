6705151e466c615520243a81bf43966fdf54dee3
Yakshit chhipa <yakshitchhipa@gamil.com>
2024-01-05 08:04:17 +0530

    test: updated tests

--- api/v1/new/PUT.js
+++
@@ -1,3 +0,0 @@
-export default function(req, res) {
-    res.json("THIS IS A PUT REQUEST HANDLER !");
-}
--- api/v1/user/DELETE.js
+++
@@ -1,3 +0,0 @@
-export default function(req, res) {
-    res.json("THIS IS A DELETE REQUEST HANDLER !");
-}
