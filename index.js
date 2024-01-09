bdb2e54121cd501190f1a10dd2447c65b78b0799
Yakshit chhipa <yakshitchhipa@gamil.com>
2024-01-09 15:41:20 +0530

    feat: start message, swc opts, transpilation on server start

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import chalk from "chalk";
-import { chconf, errorRed, whiteMessage } from "./src/chconf.js"
+import { chconf, errorRed, infoGreyDev, whiteMessage } from "./src/chconf.js"
 import nodemon from "nodemon";
 import swc from "@swc/core"
-import { existsSync, writeFileSync, mkdirSync, appendFileSync, readdirSync } from "fs";
+import { existsSync, writeFileSync, mkdirSync, readdirSync, unlinkSync } from "fs";
 import { revalidateCache } from "./src/cache.js";
 
 const config = await chconf()
@@ -14,6 +14,15 @@ if(config.TS){
     if(!existsSync(temp)){
         mkdirSync(temp, { recursive: true })
     } 
+
+    // transpile all ts files ar server start 
+    readdirSync(config.rootEndPoint).forEach(dir => {
+        readdirSync(config.rootEndPoint+"/"+dir).forEach(file => {
+            let rtf = "/"+dir+"/"+file
+            if(rtf.length > 3) 
+                transpileTs(rtf, false)
+        })
+    })
 }
 
 const packageVersion = "1.0.3"
@@ -25,13 +34,6 @@ const scriptToRun =
     config.resolvePath+"/src/dev.js"
 ;
 
-readdirSync(config.rootEndPoint).forEach(dir => {
-    readdirSync(config.rootEndPoint+"/"+dir).forEach(file => {
-        let rtf = "/"+dir+"/"+file
-        transpileTs(rtf, false)
-    })
-})
-
 await nodemon({
     script: scriptToRun,
     ext: "js,ts", // config.TS ? "ts" : "js",  // File extensions to watch for changes
@@ -39,28 +41,35 @@ await nodemon({
 })
 
 .on('restart', async (file) => {
-
     // nodemon restarts the server whenever:
     //  @ There is change in the files in the rootEndPoint dir
     //  @ There is change in the files in the cache dir
     // -------------------------------------------------------------------------------------- //
+    let t = file[0].replaceAll("\\","/").split(config.rootEndPoint)
     // if changed file was in the cache then "t" would have length 1, as there will be no split
     // we get a array of two 1st val is c/user../rootEndPonit and 2nd val is the dir/file that was changed
-    let t = file[0].replaceAll("\\","/").split(config.rootEndPoint)
-
-    // let temp = t[1].replaceAll("/","")
-    // appendFileSync(config.resolvePath+"src/cache.Hint.js", `let ${temp} = false `) // false means don't cache 
-
-    if(config.TS && t.length > 1)
+    if(config.TS && t.length > 1){
+        try {
+            let file = `cache/${t[1].toLowerCase().replaceAll("/",".").replaceAll("ts","js")}`
+            if(existsSync(file))
+                unlinkSync(file)
+        } 
+        catch (error) {infoGreyDev("At index nodemon restart: ",error)}
         transpileTs(t[1], true)
+    }
+
+    if(t.length < 2){
+        errorRed("cachw was changed !!")
+    }
 });
 
 console.log(chalk.gray(`
 ${chalk.bold.rgb(98, 0, 255)(`✦  Blazze.js v.${packageVersion} `)}
-${greenArrow()} Local: ${chalk.cyanBright(`http://localhost:${config.port}/${config.rootEndPoint}`)}
-${greenArrow()} Config: blaze.config.js
-${greenArrow()} Running in ${chalk.cyanBright(config.TS ? "TypeScript" : "JavaScript")}
-`))
+    ${greenArrow()} Local: ${chalk.cyanBright(`http://localhost:${config.port}/${config.rootEndPoint}`)}
+    ${greenArrow()} Config: blaze.config.js
+    ${greenArrow()} Request Caching: ${config.cacheRequests ? "enabled" : "disabled"}
+    ${greenArrow()} Running in ${chalk.cyanBright(config.TS ? "TypeScript" : "JavaScript")}
+`));
 
 revalidateCache("Revalidating at server start")
 
@@ -70,24 +79,23 @@ function greenArrow(){
 
 async function transpileTs(routeToTsFile,logMesBool) {
     let 
-        // t = routeToTsFile[0].replaceAll("\\","/").split(config.rootEndPoint),
         start = performance.now(),
         jsFromTs
     ;
 
-    logMesBool ?? whiteMessage(chalk.cyanBright("o"),"Blaze Transpiling TypeScript /", routeToTsFile)
+    if(logMesBool) whiteMessage(chalk.cyanBright("o"),"Blaze Transpiling TypeScript /", routeToTsFile)
 
     try {
 
         jsFromTs = await swc.transformFile(config.rootEndPoint + routeToTsFile,{
-            swcrc:true
+            swcrc:true,
+            filename:routeToTsFile
         })
 
         writeFileSync(`blazze/${routeToTsFile.replaceAll("/",".").replace("ts","js")}`, jsFromTs.code)
-        // writeFileSync(config.rootEndPoint+t[1].replace("ts","js"), jsFromTs.code)
 
         let end = performance.now()
-        logMesBool ?? whiteMessage(chalk.greenBright("✓"),`Ready in ${chalk.greenBright((end - start).toFixed(2))} ms`)
+        if(logMesBool) whiteMessage(chalk.greenBright("✓"),`Ready in ${chalk.greenBright((end - start).toFixed(2))} ms`)
     } 
 
     catch (error) {
