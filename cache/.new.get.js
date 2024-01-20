d85c041eee53785e14477e9613fc86713d74f38d
Yakshit chhipa <yakshitchhipa@gamil.com>
2024-01-20 08:05:48 +0530

    feat: request caching

---
+++ cache/.new.get.js
@@ -0,0 +1,11 @@
+export const cache = {
+    "Key": {
+        "query": {
+            "uid": "zzzzzzzzzzzzzzzzzzzzzz"
+        },
+        "params": {
+            "ids": "345dfgh"
+        }
+    },
+    "Value": "{\"Mes\":\"Dynamic nested route test with swc.\",\"Status\":\"ok\",\"Code\":200,\"Query\":{\"uid\":\"zzzzzzzzzzzzzzzzzzzzzz\"},\"Params\":{\"ids\":\"345dfgh\"}}"
+}
